2afff7062adfdcde34d2dcd93c02cd8000db87f4
RafaelaGGuimaraes <rafaelagg22@gmail.com>
2025-02-06 21:56:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
+const gridContainer = document.querySelector('.grid-container');
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
@@ -22,13 +23,36 @@ function displayResults(result) {
     resultArtist.classList.remove('hidden');
 }
 
+function displayNoResults() {
+    gridContainer.innerHTML = 
+    ` 
+    <div class="no-results">
+        <div class="no-results__content-title">
+            <h1 class="no-results__title">Oops!</h1>
+            <div class="circle">
+                <span class="fa-solid fa-x"></span>
+            </div>
+        </div>
+        <p class="no-results__subtitle">Não achamos nenhum artista com esse nome.</p>
+    </div>
+    `;
+    resultArtist.classList.remove('hidden');
+    resultPlaylist.classList.add('hidden');
+}
+
 document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+        gridContainer.innerHTML = '';        
         return
     }
     
     requestApi(searchTerm);
 })
+
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('lilacat-theme');
+    document.querySelector('.toggle-ball').classList.toggle('active');
+  });
